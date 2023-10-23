ea46591bd24a134db400695c5da115ac8c75fa7b
an-ivannikov <info@ivannikov.pro>
2023-10-23 20:29:01 +0400

    Update WorldMillionaireChallengeTimelock.sol

--- contracts/WorldMillionaireChallengeTimelock.sol
+++ contracts/WorldMillionaireChallengeTimelock.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract WorldMillionaireChallengeTimelock is TimelockController {
-    uint256 private _minDelay = 60; // TODO seconds 3*24*60*60=259200
+    uint256 private _minDelay = 259200; // TODO seconds 3*24*60*60=259200
     // TODO Multisig wallet
     address[] private _proposers = [address(0)];
     address[] private _executors = [address(0)];
